38f7b888de51cd251615c1d7473c20f12a22e2d3
ismaelresp <ismaelresp@telefonica.net>
2017-02-13 22:13:37 +0100

    fixed missing require update-package-repo before package installs and other

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,14 +3,14 @@ Exec {
     path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 }
 
+include bootstrap
 include python
 include apache
-include bootstrap
-include tools
-include adminer
 include postgresql
-include solr
 include import_db
+include adminer
+include solr
 include rdkit
 include mdsrv
 include clean_cache
+include tools
